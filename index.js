2bdc9993cc2c03b1fa4da3995d51e789e15141dc
dimitarkolev <dk@almero.bg>
2023-02-01 12:37:50 +0000

    Changed plugin interface

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-import { onProcess, useLogger, useOperations, constants } from 'mikser-core'
 import archieml from 'archieml'
 
-onProcess(() => {
-    const logger = useLogger()
-    const entities = useOperations([constants.OPERATION_CREATE, constants.OPERATION_UPDATE])
-    .map(operation => operation.entity)
-    .filter(entity => entity.content && entity.format == 'aml')
-
-    for (let entity of entities) {
-        entity.meta = Object.assign(entity.meta || {}, archieml.load(entity.content))
-        delete entity.content
-        logger.trace('ArchieML %s: %s', entity.collection, entity.id)
-    }
-})
+export default ({ 
+    onProcess, 
+    useLogger, 
+    useOperations, 
+    constants 
+}) => {
+    onProcess(() => {
+        const logger = useLogger()
+        const entities = useOperations([constants.OPERATION_CREATE, constants.OPERATION_UPDATE])
+        .map(operation => operation.entity)
+        .filter(entity => entity.content && entity.format == 'aml')
+    
+        for (let entity of entities) {
+            entity.meta = Object.assign(entity.meta || {}, archieml.load(entity.content))
+            delete entity.content
+            logger.trace('ArchieML %s: %s', entity.collection, entity.id)
+        }
+    })
+}
