fffa0de1eba1ee30cba6a3af067080d8dde07250
dimitarkolev <dk@almero.bg>
2023-02-09 14:46:01 +0000

    Journal fix

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ export default ({
 }) => {
     onProcess(() => {
         const logger = useLogger()
-        const entities = useJournal(OPERATION.CREATE, OPERATION.UPDATE)
-        .map(operation => operation.entity)
-        .filter(entity => entity.content && entity.format == 'aml')
-    
-        for (let entity of entities) {
-            entity.meta = Object.assign(entity.meta || {}, archieml.load(entity.content))
-            delete entity.content
-            logger.trace('ArchieML %s: %s', entity.collection, entity.id)
+            
+        for (let { entity } of useJournal(OPERATION.CREATE, OPERATION.UPDATE)) {
+            if (entity.content && entity.format == 'aml') {
+                entity.meta = Object.assign(entity.meta || {}, archieml.load(entity.content))
+                delete entity.content
+                logger.trace('ArchieML %s: %s', entity.collection, entity.id)
+            }
         }
     })
 }
