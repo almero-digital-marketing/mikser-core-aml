19cd784fbfcf5f3da2ef03ffcdf4370238998b8b
dimitarkolev <dk@almero.bg>
2023-03-27 08:16:54 +0000

    Bump

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ export default ({
     onProcess, 
     useLogger, 
     useJournal, 
+    updateEntry,
     constants: { OPERATION }
 }) => {
-    onProcess(() => {
+    onProcess(async () => {
         const logger = useLogger()
             
-        for (let { entity } of useJournal(OPERATION.CREATE, OPERATION.UPDATE)) {
+        for await (let { id, entity } of useJournal('Aml', [OPERATION.CREATE, OPERATION.UPDATE])) {
             if (entity.content && entity.format == 'aml') {
                 entity.meta = Object.assign(entity.meta || {}, archieml.load(entity.content))
                 delete entity.content
+                await updateEntry({ id, entity })
                 logger.trace('ArchieML %s: %s', entity.collection, entity.id)
             }
         }
